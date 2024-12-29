6c0e031f857cb0fa6f040f8936253caaeb96bae9
Sarwar Jahan <sarwar3@gmail.com>
2024-12-29 11:00:30 +0600

    Nav bar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,44 @@
 </head>
 
 <body>
+    <!-- nav bar-->
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+        < class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sales</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Employees</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+    </nav>
+
+    <nav class="navbar bg-primary" data-bs-theme="dark">
+        <!-- Navbar content -->
+    </nav>
+
+    <nav class="navbar" style="background-color: #e3f2fd;">
+        <!-- Navbar content -->
+    </nav>
+    <!-- nav bar ends-->
+
 
 
     <!-- creating table  -->
